6314ba73356b831440fca7867bfdadfcc2f2f277
Alan Contrera <giovannicontre24@gmail.com>
2026-09-14 12:26:58 -0600

    feat(infraestructura): configurar servicio nginx como reverse proxy y web server

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -115,6 +115,7 @@ USE_TZ = True
 
 # --- ARCHIVOS ESTATICOS ---
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # --- ARCHIVOS MULTIMEDIA (adjuntos) ---
